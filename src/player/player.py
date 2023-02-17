d08f3bfb9da5a29b3ec81361710bd112037b06c7
cooki <cookiedoughtrain@gmail.com>
2023-02-16 23:57:20 -0800

    jupyter notebook conversion + cleaning data for player.py

---
+++ src/player/player.py
@@ -0,0 +1,29 @@
+'''
+Player Analysis for BPL 10
+
+Author: Cearlmau
+
+This file breaks down the player distribution of BPL teams based on team and level
+at various points in the BPL 10 event. The goal of this data analysis is to look at
+whether or not the team distributions had an effect on overall team player levels, and
+how that may contribute to a team's success.
+
+Special thanks to Moowiz for providing the data.
+'''
+
+import csv
+import pandas as pd
+
+level_data = pd.read_csv("../../data/bpl-10_level_data.csv")
+teams_data = pd.read_csv("../../data/bpl-10_teams.csv")
+
+print(teams_data.to_string())
+
+#Team Analysis
+royals_data = 
+
+
+if __name__ == "__main__":
+    print("hi")
+
+
